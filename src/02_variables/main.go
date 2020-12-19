a36bbde48615b0643b30d63807ae4409f33ea4bc
noobmaster-007 <yongshuowang007@gmail.com>
2020-12-19 21:29:05 +0800

    Add step2

--- src/02_variables/main.go
+++ src/02_variables/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	// TODO: Create an integer that has range 0 to 255 ONLY with default value 128. See what you get if you change the default value to 256
+
+	// TODO: Create a 32 bits floating number with value 3.14
+
+	// TODO: Using short variable to create a string with value "Hello"
+
+	fmt.Println("END")
 }
